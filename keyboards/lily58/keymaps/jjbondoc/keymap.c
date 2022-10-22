52e560801aa0adba05d3a9c15d00cca1f481dd76
junel <101399159+jjbondoc@users.noreply.github.com>
2022-10-23 00:16:15 +0100

    update gaming layer

--- keyboards/lily58/keymaps/jjbondoc/keymap.c
+++ keyboards/lily58/keymaps/jjbondoc/keymap.c
@@ -59,17 +59,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB, 	KC_Q, 	  KC_W, 	KC_E, 	  KC_R, 	KC_T, 						  KC_Y, 	KC_U, 	  KC_I,     KC_O, 	  KC_P,     KC_EQL,
 		KC_LSFT, 	KC_A,     KC_S, 	KC_D, 	  KC_F, 	KC_G, 						  KC_H, 	KC_J, 	  KC_K, 	KC_L, 	  KC_SCLN,  KC_QUOT,
 		KC_LCTL, 	KC_Z, 	  KC_X, 	KC_C, 	  KC_V, 	KC_B,     KC_NO,    KC_NO, 	  KC_N, 	KC_M, 	  KC_COMM,  KC_DOT,   KC_SLSH,  TG(4),
-										KC_LGUI,  KC_LALT,  KC_SPC,   MO(5),    KC_NO,    KC_BSPC,  KC_ENT,   KC_RSFT
+										KC_LGUI,  KC_LALT,  KC_SPC,   MO(5),    MO(5),    KC_BSPC,  KC_ENT,   KC_RSFT
 		//-------------------------------------------------------------------|---------------------------------------------------------------------- 
 	),
 
 	[5] = LAYOUT(
 		//-------------------------------------------------------------------|---------------------------------------------------------------------- 
-		KC_TRNS, 	KC_F1, 	  KC_F2, 	KC_F3, 	  KC_F4, 	KC_F5, 						  KC_NO, 	KC_NO,    KC_NO,    KC_NO,    KC_NO, 	KC_NO,
-		KC_ESC,     KC_6, 	  KC_7, 	KC_8, 	  KC_9, 	KC_0, 						  KC_NO, 	KC_NO, 	  KC_NO, 	KC_NO, 	  KC_NO, 	KC_NO,
-		KC_TRNS, 	KC_1, 	  KC_2, 	KC_3, 	  KC_4, 	KC_5, 						  KC_NO, 	KC_NO, 	  KC_NO, 	KC_NO, 	  KC_NO, 	KC_NO,
-		KC_TRNS, 	KC_F6, 	  KC_F7, 	KC_F8, 	  KC_F9, 	KC_F10,   KC_F11,   KC_F12,   KC_NO, 	KC_NO, 	  KC_NO, 	KC_NO, 	  KC_NO, 	KC_NO,
-										KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_NO, 	  KC_NO, 	KC_NO,    KC_NO
+		KC_TRNS, 	KC_F1, 	  KC_F2,    KC_F3,    KC_F4,    KC_F5, 						  KC_F6, 	KC_F7, 	  KC_F8, 	KC_F9, 	  KC_F10,   KC_F11,
+		KC_ESC, 	KC_F13,   KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_TRNS, 					  KC_PERC,  KC_LCBR,  KC_RCBR,  KC_CIRC,  KC_AMPR,  KC_F12,
+		KC_TRNS, 	KC_F14,   KC_VOLD,  KC_MUTE,  KC_VOLU,  KC_TRNS, 					  KC_HASH,  KC_LPRN,  KC_RPRN,  KC_UNDS,  KC_AT, 	KC_TRNS,
+		KC_TRNS, 	KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_NO,    KC_NO,    KC_DLR,   KC_LBRC,  KC_RBRC,  KC_NUBS,  KC_TRNS,  KC_GRV,
+										KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_DEL,   KC_TRNS
 		//-------------------------------------------------------------------|---------------------------------------------------------------------- 
 	)
 };
